8c2eca542066213dc2f2c5ef7916c97a9faad8e6
Arjun31415 <arjunp0710@gmail.com>
2025-10-02 20:26:08 +0200

    chore:upd comment

--- src/custom_types.py
+++ src/custom_types.py
@@ -1,4 +1,4 @@
 # Type aliases for clarity
-Bid = tuple[int,float, float]  # (price, quantity)
-Offer = tuple[int,float, float]  # (price, quantity)
+Bid = tuple[int, float, float]  # (agent_id, price, quantity)
+Offer = tuple[int, float, float]  # (agent_id, price, quantity)
 MarketResult = tuple[float, float]  # (clearing_price, clearing_quantity)
