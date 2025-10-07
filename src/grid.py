cc1c770184a4960ad53b587b1cb1056149d57757
Arjun31415 <arjunp0710@gmail.com>
2025-10-07 14:52:32 +0200

    feat!: added time horizon for simulator. Each agent submits bids for next time_horizon steps (say 24 hours) and gets the info about the predicted clearing price based on this submission. Also added solar and wind generation profiles

--- src/grid.py
+++ src/grid.py
@@ -16,64 +16,73 @@ class Grid:
             if trade[2] == 0:
                 continue
 
-            path = nx.shortest_path(self.graph, trade[0], 'Transformer')
+            path = nx.shortest_path(self.graph, trade[0], "Transformer")
             path_edges = list(zip(path, path[1:]))
 
             for edge in path_edges:
                 if edge[1] == "Transformer":
-                    new_flow = self.graph.nodes["Transformer"]['flow'] - trade[2]
-                    if abs(new_flow) > self.graph.nodes["Transformer"]['capacity']:
+                    new_flow = self.graph.nodes["Transformer"]["flow"] - trade[2]
+                    if abs(new_flow) > self.graph.nodes["Transformer"]["capacity"]:
                         raise Exception("New flow exceeds transformer capacity")
-                    self.graph.nodes["Transformer"]['flow'] = new_flow
+                    self.graph.nodes["Transformer"]["flow"] = new_flow
                     continue
 
-                new_flow = self.graph[edge[0]][edge[1]]['flow'] - trade[2]
-                if abs(new_flow) > self.graph[edge[0]][edge[1]]['capacity']:
+                new_flow = self.graph[edge[0]][edge[1]]["flow"] - trade[2]
+                if abs(new_flow) > self.graph[edge[0]][edge[1]]["capacity"]:
                     raise Exception("New flow exceeds capacity")
-                self.graph[edge[0]][edge[1]]['flow'] = new_flow
+                self.graph[edge[0]][edge[1]]["flow"] = new_flow
 
-            path = nx.shortest_path(self.graph, 'Transformer', trade[1])
+            path = nx.shortest_path(self.graph, "Transformer", trade[1])
             path_edges = list(zip(path, path[1:]))
 
             for edge in path_edges:
-                if edge[0] == 'Transformer':
-                    new_flow = self.graph.nodes["Transformer"]['flow'] + trade[2]
-                    if abs(new_flow) > self.graph.nodes["Transformer"]['capacity']:
+                if edge[0] == "Transformer":
+                    new_flow = self.graph.nodes["Transformer"]["flow"] + trade[2]
+                    if abs(new_flow) > self.graph.nodes["Transformer"]["capacity"]:
                         raise Exception("New flow exceeds transformer capacity")
-                    self.graph.nodes["Transformer"]['flow'] = new_flow
+                    self.graph.nodes["Transformer"]["flow"] = new_flow
                     continue
 
-                new_flow = self.graph[edge[0]][edge[1]]['flow'] + trade[2]
-                if abs(new_flow) > self.graph[edge[0]][edge[1]]['capacity']:
+                new_flow = self.graph[edge[0]][edge[1]]["flow"] + trade[2]
+                if abs(new_flow) > self.graph[edge[0]][edge[1]]["capacity"]:
                     raise Exception("New flow exceeds capacity")
-                self.graph[edge[0]][edge[1]]['flow'] = new_flow
+                self.graph[edge[0]][edge[1]]["flow"] = new_flow
 
     def retrieve_capacity(self, n: str, u: str) -> int:
         """Finds the maximum capacity trade from node u to u"""
 
-        path = nx.shortest_path(self.graph, n, 'Transformer')
+        path = nx.shortest_path(self.graph, n, "Transformer")
         path_edges = list(zip(path, path[1:]))
 
-        flow = float('inf')
-        if n == 'Transformer' or u == 'Transformer':
-            flow = self.graph.nodes["Transformer"]['capacity'] - self.graph.nodes["Transformer"]['flow']
+        flow = float("inf")
+        if n == "Transformer" or u == "Transformer":
+            flow = (
+                self.graph.nodes["Transformer"]["capacity"]
+                - self.graph.nodes["Transformer"]["flow"]
+            )
 
         for edge in path_edges:
-            if edge[0] == "Transformer" or edge[1] == 'Transformer':
+            if edge[0] == "Transformer" or edge[1] == "Transformer":
                 continue
 
-            cap = self.graph[edge[0]][edge[1]]['capacity'] + self.graph[edge[0]][edge[1]]['flow']
+            cap = (
+                self.graph[edge[0]][edge[1]]["capacity"]
+                + self.graph[edge[0]][edge[1]]["flow"]
+            )
             if cap < flow:
                 flow = cap
 
-        path = nx.shortest_path(self.graph, 'Transformer', u)
+        path = nx.shortest_path(self.graph, "Transformer", u)
         path_edges = list(zip(path, path[1:]))
 
         for edge in path_edges:
-            if edge[0] == "Transformer" or edge[1] == 'Transformer':
+            if edge[0] == "Transformer" or edge[1] == "Transformer":
                 continue
 
-            cap = self.graph[edge[0]][edge[1]]['capacity'] - self.graph[edge[0]][edge[1]]['flow']
+            cap = (
+                self.graph[edge[0]][edge[1]]["capacity"]
+                - self.graph[edge[0]][edge[1]]["flow"]
+            )
             if cap < flow:
                 flow = cap
 
@@ -108,16 +117,16 @@ def read_grid(read_file: str) -> Grid:
 
         nodes = text[1][1:-1].split(",")
         edges = text[3][1:-1].split(",")
-        transformer = nodes[0][1:-1].split(';')
+        transformer = nodes[0][1:-1].split(";")
         if transformer[0] != "Transformer" or not transformer[1].isdigit():
             raise Exception("Incorrect transformer format")
         nodes = nodes[1:]
 
         def transform(x):
             text = x[1:-1].split(";")
-            if not text[2].isdigit() and text[2] != '-1':
+            if not text[2].isdigit() and text[2] != "-1":
                 raise Exception("capacity needs to be an integer")
-            return text[0], text[1], {'capacity': int(text[2]), 'flow': 0}
+            return text[0], text[1], {"capacity": int(text[2]), "flow": 0}
 
         edges = list(map(transform, edges))
         g = nx.Graph()
