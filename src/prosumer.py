c331e41eb0fbe182ad9bda6e8f2703e91aec46ad
Arjun31415 <arjunp0710@gmail.com>
2025-10-23 11:40:09 +0200

    fix: bug in minimum price for seller

--- src/prosumer.py
+++ src/prosumer.py
@@ -247,7 +247,7 @@ class ProsumerAgent:
                 price = base_price * price_noise * (1 - 0.1 * (0.91) ** nd)
                 price = np.clip(price, action_space.low[h, 0], action_space.high[h, 0])
                 qty = np.clip(abs(nd), action_space.low[h, 1], action_space.high[h, 1])
-                price = max(price, self.price_per_unit*qty)
+                price = max(price, self.price_per_unit)
                 offers[h] = [price, qty]
                 bids[h] = [0, 0]
 
